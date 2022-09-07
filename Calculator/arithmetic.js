f0a35b043a2aa770cc12d488f3c7e6154f0a98bc
ujjwalAthena <ujjwal.kushwaha@athenalogics.com>
2022-09-07 18:26:48 +0530

    calc partially worked

--- Calculator/arithmetic.js
+++
@@ -1,4 +0,0 @@
-function add(a,b) {
-    document.getElementById("demo").value= ;
-
-}
--- Calculator/index.js
+++ Calculator/index.js
@@ -1,75 +1,90 @@
 
 
-function add(a,b){
-    return a+b
+function ff(){
+
+    var exp=document.getElementById("demo").value;
+    var res=eval(exp);
+    // console.log(res);
+    document.getElementById("demo1").value="=" +res;
 }
 
+
+
+
+
 function s7() {
-    document.getElementById("demo").value= 7 ;
+    document.getElementById("demo").value+= 7 ;
 
 }
 
 function s8() {
-    document.getElementById("demo").value=8;
+    document.getElementById("demo").value+=8;
 
 }
+
 function s9() {
-    document.getElementById("demo").value=9;
+    document.getElementById("demo").value+=9;
 
 }
 function s4() {
-    document.getElementById("demo").value=4;
+    document.getElementById("demo").value+=4;
 
 }
 function s5() {
-    document.getElementById("demo").value=5;
+    document.getElementById("demo").value+=5;
 
 }
 function s6() {
-    document.getElementById("demo").value=6;
+    document.getElementById("demo").value+=6;
 
 }
 function s1() {
-    document.getElementById("demo").value=1;
+    document.getElementById("demo").value+=1;
 
 }
 function s2() {
-    document.getElementById("demo").value=2;
+    document.getElementById("demo").value+=2;
 
 }
 function s3() {
-    document.getElementById("demo").value=3;
+    document.getElementById("demo").value+=3;
 
 }
 
 function sa() {
-    document.getElementById("demo").value= "+" ;
+    document.getElementById("demo").value+= "+";
 
 }
 function ss() {
-    document.getElementById("demo").value= "-" ;
+    document.getElementById("demo").value+= "-" ;
 
 }
 
 function sm() {
-    document.getElementById("demo").value= "*";
+    document.getElementById("demo").value+= "*";
 
 }
-function sd() {
-    document.getElementById("demo").value="/";
 
-}
-function se() {
-    document.getElementById("demo").value= "=";
+// function se() {
+//     document.getElementById("demo").value= ff();
 
-}
+// }
+
 function so() {
-    document.getElementById("demo").value= "0";
-
-}function sd() {
-    document.getElementById("demo").value= ".";
+    document.getElementById("demo").value+= "0";
 
 }
+
+function sd() {
+    document.getElementById("demo").value+= ".";
+
+}
+
+function sc() {
+    document.getElementById("demo").value= "";
+
+}
+
 
 
 
